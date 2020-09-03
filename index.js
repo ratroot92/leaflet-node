c11f31d2af81b916d420ba6e8520d6d230369f61
ratroot92 <zcomisb.007@gmail.com>
2020-09-03 22:01:09 +0500

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const DB = require("./config/db");
 const expressValidator = require("express-validator");
+const cors = require("cors");
 
 // var connection = mysql.createPool({
 //   host: "deikho-billing.cwf1fb8z5zka.us-east-1.rds.amazonaws.com",
@@ -21,13 +22,33 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use(expressValidator())
+const corsOpts = {
+  origin: '*',
 
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts));
 // register router
-const userRouter = require("./routes/user.routes");
-app.use("/testapi", userRouter);
+app.post('/save',(req,res)=>{
+console.log(req.body)
+})
 
 //Start server
 const port = process.env.PORT || "9000";
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Listening to requests on http://0.0.0.0:${port}`);
+app.listen(port ,(err)=> {
+  if(err){
+    console.log(err)
+  }
+  else{
+    console.log(`Listening to requests on http://0.0.0.0:${port}`);
+  }
+  
 });
